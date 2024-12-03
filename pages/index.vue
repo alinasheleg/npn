30a38ccbce8289b1a636a662e4912ed874583bb4
ИС-23-19Б <@POLYTECH2.KZ>
2024-12-03 13:42:43 +0500

    filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,55 @@
-<script lang='ts'>
+<script setup lang='ts'>
+const filmStore = useFilmStore();
+const countryStore = useCountryStore();
+const categoryStore = useCategoryStore();
+
+const category = ref(null);
+const country = ref(null);
+const sort = ref('name');
+const filter = () => {
+  filmStore.addCategoryToParams(category.value);
+  filmStore.addCountryToParams(category.value);
+  filmStore.addSortToParams(category.value);
+  filmStore.fetchFilms();
+}
+
+const resetFilter = () => {
+  category.value = null;
+  country.value = null;
+  sort.value = 'name';
+  filter();
+
+
+}
+
 
 </script>
 
 <template>
-  <div class="row">
-    <div class="col">
-      <select class="form-select" aria-label="Default select example">
-        <option selected>Select genge</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+  <div class="mt-3 row">
+
+    <div class="col-md-4">
+      <select v-model="category" @change="filter" class="form-select" aria-label="Default select example">
+        <option :value="null" selected>Select genge</option>
+        <option
+            v-for="category in categoryStore.categories"
+            :key="category.id"
+            :value="category.id">{{ category.name }} ({{ category.filmCount }})</option>
       </select>
     </div>
 
-    <div class="col">
-      <select class="form-select" aria-label="Default select example">
+    <div class="col-md-4">
+      <select v-model="country" @change="filter" class="form-select" aria-label="Default select example">
+        <option :value="null" selected>Select country</option>
+        <option
+            v-for="country in countryStore.countries"
+            :key="country.id"
+            :value="country.id"
+        >{{ country.name }}</option>
+      </select>
+    </div>
+    <div class="col-md-2">
+      <select v-model="sort" @change="filter" class="form-select" aria-label="Default select example">
         <option selected>Select genge</option>
         <option value="Name">Name</option>
         <option value="Year">Year</option>
@@ -22,60 +57,44 @@
       </select>
     </div>
 
-    <div class="col">
-      <button  class="btn outline-warning">Reseyt</button>
+    <div class="col-md-2">
+      <button @click="resetFilter" class="btn outline-warning">Reset</button>
     </div>
+    <div  class="row row-cols-1 row-cols-md-3 g-4">
 
-    <div class="col">
+      <div class="col" v-for="film in filmStore.films" :key="film.id" >
+        <div class="card">
+          <img v-if="film.link_img" :src="film.link_img" class="card-img-top" alt="film.link_img">
+          <img v-else src="https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/50c92dd6-51a3-4ec5-a334-fc21f486e0bc/1920x" class="card-img-top" alt="film.link_img">
+          <div class="card-body">
+            <h5 class="card-title">{{film.name}}</h5>
+            <p class="card-text">{{ film.ratingAvg }}</p>
+            <p class="card-text">{{ film.duration }} min.</p>
+            <p class="card-text">
+              < class="card-title"><template v-for="category in film.categories" :key = "category.id">{{category.name}}>
+                {{category.name}}
+              </template>
+            </p>
+            <button  type="button" class="btn btn-info">Info</button>
 
-    </div>
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col">
-        <div class="card">
-          <img src="https://i.pinimg.com/originals/43/1c/0b/431c0b781e2feae665a8c3b765fd1cc9.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Ужасающий 2</h5>
-            <p class="card-text">7.2</p>
-            <p class="card-text">2ч 18 мин</p>
-            <p class="card-text">Ужасы</p>
-
-
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="https://avatars.mds.yandex.net/i?id=dc654eedbff8b69ee107ec70d9759fa3_l-9700166-images-thumbs&n=13" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Ужасающий 2</h5>
-            <p class="card-text">7.2</p>
-            <p class="card-text">2ч 18 мин</p>
-            <p class="card-text">Ужасы</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="https://s2.afisha.ru/mediastorage/ff/b1/e88051d8e8b3439397a42c88b1ff.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Ужасающий 3</h5>
-            <p class="card-text">7.2</p>
-            <p class="card-text">2ч 5 мин</p>
-            <p class="card-text">Ужасы</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          <img src="http://images-s.kinorium.com/movie/poster/10469877/w1500_52803308.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Ужасающий 3</h5>
-            <p class="card-text">7.2</p>
-            <p class="card-text">2ч 5 мин</p>
-            <p class="card-text">Ужасы</p>
           </div>
         </div>
       </div>
     </div>
   </div>
+  <div class="justify-content-center">
+
+    <div v-if="filmStore.isLoading" class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  <nav class="d-flex justify-content-center my-4" aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+    </ul>
+  </nav>
 </template>
--- stores/film.js
+++ stores/film.js
@@ -3,10 +3,45 @@ import { api } from "~/api/index.js";
 
 export const useFilmStore = defineStore("film", () => {
     const films = ref([]);
+    const isLoading = ref(false);
+    const params = ref({
+        page: 1,
+        size: 10,
+        sortBy: "name",
+        sortDirection: "asc",
+        category: null,
+        country: null,
+
+    });
+
+    const  addCategoryToParams = (category) => {
+        params.value.category = category;
+
+
+    }
+
+    const  addCountryToParams = (country) => {
+        params.value.country = country;
+
+
+    }
+
+
+    const  addSortToParams = (sort) => {
+        params.value.sortBy = sort;
+
+
+    }
+
 
     const fetchFilms = async () => {
-        const res = await api.get('/films');
+        isLoading.value = true;
+        const res = await api.get('/films', {
+                params: params.value,
+            }
+        );
         films.value = res.data.films;
+        isLoading.value = false;
     };
 
     fetchFilms();
@@ -14,5 +49,9 @@ export const useFilmStore = defineStore("film", () => {
     return {
         films,
         fetchFilms,
+        isLoading,
+        addSortToParams,
+        addCountryToParams,
+        addCategoryToParams,
     };
 });
