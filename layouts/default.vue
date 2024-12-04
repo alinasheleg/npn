864e646877fe2bf3d4cc0c231b229f7a8b4e5143
ИС-23-19Б <@POLYTECH2.KZ>
2024-12-04 13:38:50 +0500

    feat: add reg page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,26 @@
 <template>
-  <div>
-  </div>
 
 
   <header>
     <!-- Fixed navbar -->
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Kino</a>
+        <a class="navbar-brand" href="#">KinoTower</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav me-auto mb-2 mb-md-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+
+              <NuxtLink class="nav-link active" to="/">Home</NuxtLink>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <button type="button" class="btn btn-secondary">Sign in</button>
-            <button type="button" class="btn btn-secondary">Sign up</button>
+          <div class="ms-auto d-flex">
+            <NuxtLink to="/signin"><button class="btn btn-dark" type="button" >Sign In</button></NuxtLink>
+            <NuxtLink to="/signup"><button class="btn btn-dark" type="button" >Sign Up</button></NuxtLink>
+          </div>
 
-          </form>
         </div>
       </div>
     </nav>
@@ -40,11 +33,11 @@
     </div>
   </main>
 
-  <footer class="footer mt-auto py-3 bg-body-tertiary">
-    <div class="container">
-      <span class="text-body-secondary">Place sticky footer content here.</span>
-    </div>
-  </footer>
+  <!--  <footer class="fixed-bottom">-->
+  <!--    <div class="container">-->
+  <!--      <span class="text-body-secondary">KinoTower-2024.</span>-->
+  <!--    </div>-->
+  <!--  </footer>-->
 
 
 </template>
